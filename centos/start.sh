44e9d1e9415fba861f66bc1227c482de9a4847a4
Shaun Martin <shaun@samsite.ca>
2017-08-09 17:02:48 -0500

    initial commit

---
+++ centos/start.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+### startup script for Ansible testing
+# NOTE: Set working directory with `-w` in the `docker run` command.
+
+verbosity=''
+playbook=''
+galaxyfile='requirements.yml'
+pypifile='requirements.txt'
+inventory='localhost,'
+
+USAGE="""$0 [-p test.yml] [-g requirements.yml] [-r requirements.txt] [-i 'localhost,'] [-v] [-h]
+  Installs pre-reqs and runs an Ansible playbook. Looks for / falls back to test.yml,
+  local.yml, and playbook.yml by default.
+
+  -p    Path to Ansible playbook
+  -g    Path to Ansible Galaxy requirements file (default: requirements.yml)
+  -r    Path to PyPI/pip requirements file (default: requirements.txt)
+  -i    Inventory string passed directly to Ansible (default: localhost,)
+  -v    Enable debug messages
+  -h    Show this help message
+"""
+
+while getopts 'p:g:r:i:hv' flag; do
+  case "${flag}" in
+    p) playbook="${OPTARG}" ;;
+    g) galaxyfile="${OPTARG}" ;;
+    r) pypifile="${OPTARG}" ;;
+    i) inventory="${OPTARG}" ;;
+    v) verbosity="-vvv" ;;
+    h) echo "${USAGE}"; exit 0 ;;
+    *) echo -e "ERROR: Unexpected option ${flag}!\n"; echo "${USAGE}"; exit 1 ;;
+  esac
+done
+
+# Install ansible-galaxy requirements
+if [ -f "${galaxyfile}" ]; then
+  echo "### Installing pre-reqs from Ansible Galaxy..."
+  ansible-galaxy install -r "${galaxyfile}"
+fi
+
+# Install Python requirements
+if [ -f "${pypifile}" ]; then
+  echo "### Installing pre-reqs from PyPI..."
+  pip install -r "${pypifile}"
+fi
+
+# Look for test.yml, falling back to local.yml and playbook.yml
+if [ ! -f "${playbook}" ]; then
+  for pb in 'test.yml' 'local.yml' 'playbook.yml'; do
+    if [ -f "${pb}" ]; then
+      playbook="${pb}"
+    fi
+  done
+fi
+
+# Do the thing.
+ansible-playbook "${verbosity}" "${playbook}" -i "${inventory}"
