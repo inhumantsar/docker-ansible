9f011ad897189eb55091ad257d1fa7176ce7ab64
Shaun Martin <shaun@samsite.ca>
2017-09-07 12:43:39 -0400

    Adding ability to passthru args to ansible-playbook

--- alpine/start.sh
+++ alpine/start.sh
@@ -10,28 +10,37 @@ pypifile='requirements.txt'
 inventory='localhost,'
 cmd="ansible-playbook"
 
-USAGE="""$0 [-p test.yml] [-g requirements.yml] [-r requirements.txt] [-i 'localhost,'] [-v] [-h]
-  Installs pre-reqs and runs an Ansible playbook. Looks for / falls back to test.yml,
-  local.yml, and playbook.yml by default.
+USAGE="""$0 [-p test.yml] [-g requirements.yml] [-r requirements.txt] [-s system_packages.txt] [-*] [-h]
+  Installs pre-reqs and runs an Ansible playbook.
 
-  -p    Path to Ansible playbook
+  -p    Path to Ansible playbook (default: test.yml > local.yml > playbook.yml > site.yml)
   -g    Path to Ansible Galaxy requirements file (default: requirements.yml)
   -r    Path to PyPI/pip requirements file (default: requirements.txt)
-  -i    Inventory string passed directly to Ansible (default: localhost,)
+  -s    Path to a list of system packages to install, one per line. (default: system_packages.txt)
+  -*    Any option supported by ansible-playbook (eg: -e SOMEVAR=someval -i /path/to/inventory)
   -v    Enable debug messages
   -h    Show this help message
 """
 
-while getopts 'p:g:r:i:hv' flag; do
-  case "${flag}" in
-    p) playbook="${OPTARG}" ;;
-    g) galaxyfile="${OPTARG}" ;;
-    r) pypifile="${OPTARG}" ;;
-    i) cmd="${cmd} -i ${OPTARG}" ;;
-    v) cmd="${cmd} -vvv" ;;
-    h) echo "${USAGE}"; exit 0 ;;
-    *) echo -e "\nERROR: Unexpected option ${flag}!\n"; echo "${USAGE}"; exit 1 ;;
-  esac
+# doing this instead of getopts so we can trap "invalid" params and use them as
+# part of the ansible-playbook command
+while test $# -gt 0; do
+    # echo "$1 $2"
+    if [ "$1" == "-p" ]; then
+      playbook="${2}" #; echo "playbook=${playbook}"
+    elif [ "$1" == "-g" ]; then
+      galaxyfile="${2}" #; echo "galaxyfile=${galaxyfile}"
+    elif [ "$1" == "-r" ]; then
+      pypifile="${2}" #; echo "pypifile=${pypifile}"
+    elif [ "$1" == "-s" ]; then
+      pkgfile="${2}" #; echo "pkgfile=${pkgfile}"
+    elif [ "$1" == "-h" ]; then
+      echo $USAGE; exit 0
+    else
+      cmd="${cmd} $1 $2" #; echo $cmd
+    fi
+
+    shift; shift
 done
 
 # Install ansible-galaxy requirements
@@ -46,9 +55,19 @@ if [ -f "${pypifile}" ]; then
   pip install -r "${pypifile}"
 fi
 
-# Look for test.yml, falling back to local.yml and playbook.yml
+# Install system packages
+if [ -f "${pkgfile}"]; then
+  echo -e "\n### Installing system packages..."
+  pkgs=""
+  cat $pkgfile | while read line; do
+    pkgs="${pkgs} ${line}"
+  done
+  apk --no-cache add $pkgs
+fi
+
+# Look for a playbook file
 if [ ! -f "${playbook}" ]; then
-  for pb in 'test.yml' 'local.yml' 'playbook.yml'; do
+  for pb in 'test.yml' 'local.yml' 'playbook.yml' 'site.yml'; do
     if [ -f "${pb}" ]; then
       playbook="${pb}"
     fi
