ae2b749ea70376754a57fc1f30318981852fe42a
Shaun Martin <sam@localhost-live.hitronhub.home>
2018-08-08 16:13:03 -0500

    more derp

--- travis_build.sh
+++ travis_build.sh
@@ -41,15 +41,6 @@ for os in "$@"; do
 
     docker build --no-cache --build-arg VERSION="${ansible_version}" -t $HUB_USER/ansible:$tag -f $os.Dockerfile . || travis_terminate 1 &> /dev/null || exit 1
 
-    # determine what actual branch we're working on. see https://graysonkoonce.com/getting-the-current-branch-name-during-a-pull-request-in-travis-ci/
-    BRANCH=$(if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then echo $TRAVIS_BRANCH; else echo $TRAVIS_PULL_REQUEST_BRANCH; fi)
-
-    # don't deploy if we're not operating on the master branch
-    if [ "${BRANCH}" != "master" ]; then 
-      echo "Cowardly refusing to deploy from any branch but master (currently on branch '${BRANCH}')"
-      exit 0
-    fi
-
 
     echo """
 
@@ -70,6 +61,15 @@ for os in "$@"; do
     echo "  - Image version is ${image_version}, expecting ${latest_git_tag}"
     test "${image_version}" == "${latest_git_tag}" || travis_terminate 1 &> /dev/null || exit 1
 
+
+    # determine what actual branch we're working on. see https://graysonkoonce.com/getting-the-current-branch-name-during-a-pull-request-in-travis-ci/
+    BRANCH=$(if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then echo $TRAVIS_BRANCH; else echo $TRAVIS_PULL_REQUEST_BRANCH; fi)
+
+    # don't deploy if we're not operating on the master branch
+    if [ "${BRANCH}" != "master" ]; then 
+      echo "Cowardly refusing to deploy from any branch but master (currently on branch '${BRANCH}')"
+      exit 0
+    fi
 
 
     echo """
